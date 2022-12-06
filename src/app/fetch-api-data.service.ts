f53126cb39f4b6e74d8e23f8f6b17cc61a4ea15a
alexapenzes <penzesalexa@gmail.com>
2022-12-06 16:43:03 +0100

    update API

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -9,7 +9,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://ap-myflix.herokuapp.com/';
+const apiUrl = 'https://movie-api-phi.vercel.app/';
 @Injectable({
   providedIn: 'root',
 })
